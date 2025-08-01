9c1110a4716c81823d793eb62575cf3e0751e5a0
Ajaypratapsingh2018 <ajaypratapsingh2018@gmail.com>
2025-08-01 20:13:56 +0530

    Testing

--- mern/backend/db/connection.js
+++ mern/backend/db/connection.js
@@ -1,24 +1,27 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
-const URI = "mongodb://mongodb:27017";
-const client = new MongoClient(URI, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+const URI = 'mongodb://mongodb:27017';
+
+async function connectDB() {
+  const client = new MongoClient(URI, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
 
 try {
-  // Connect the client to the server
-  await client.connect();
-  // Send a ping to confirm a successful connection
-  await client.db("admin").command({ ping: 1 });
-  console.log("Pinged your deployment. You successfully connected to MongoDB!");
-} catch (err) {
-  console.error(err);
+    // Connect the client to the server
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log('Pinged your deployment. You successfully connected to MongoDB!');
+    return client.db('employees');
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 }
 
-let db = client.db("employees");
-
-export default db;
+export default connectDB;
