535a78c27c5185b26de8ebf40c330faae86fe374
Ajaypratapsingh2018 <ajaypratapsingh2018@gmail.com>
2025-08-01 20:21:47 +0530

    Testing

---
+++ mern/backend/eslint.config.js
@@ -0,0 +1,20 @@
+﻿import eslintRecommended from 'eslint/conf/eslint-recommended.js';
+import globals from 'globals';
+
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.es2021
+      }
+    },
+    plugins: [],
+    rules: {
+      ...eslintRecommended.rules
+      // You can add more custom rules here
+    }
+  }
+];
